d01418b17ce2437e91a2a2efeb2b7f7f1ab3467d
sharifnezhad <sharifnezhad@amirhosseinsharifnezhad@gmail.com>
2023-09-19 21:12:53 +0330

    add http method

---
+++ src/Classes/Concerns/Methods/DeleteMethod.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace ASharifnezhad\ApiDoc\Classes\Concerns\Methods;
+
+use Mpociot\Reflection\DocBlock;
+use Mpociot\Reflection\DocBlock\Tag;
+
+class DeleteMethod extends Method
+{
+    public function __construct($config)
+    {
+        parent::__construct($config);
+    }
+
+    public function methodParams(Docblock $classData, Docblock $methodData, $customData): array
+    {
+        $this->classData = $classData;
+        $this->methodData = $methodData;
+
+        $postMethodParams = array_merge($this->setDefaultParams($customData), [
+            'requestBody' => $methodData->hasTag('bodyParam') ? $this->setParameters($methodData->getTagsByName('bodyParam')) : [],
+            'responses' => $methodData->hasTag('response') ? $this->setResponses($methodData->getTagsByName('response')) : null,
+        ]);
+        return [
+            'delete' => $postMethodParams
+        ];
+    }
+
+    public function setParameters(array $pathParams)
+    {
+        $requiredData = collect($pathParams)->filter(function (Tag $tag) {
+            $values = json_decode($tag->getContent());
+            return isset($values->required) && $values->required;
+        })->map(function (Tag $tag) {
+            $values = json_decode($tag->getContent());
+            return $values;
+        });
+
+        $propertiesData = collect($pathParams)->mapWithKeys(function (Tag $tag) {
+            $values = json_decode($tag->getContent());
+            return [
+                $values->key => [
+                    'type' => $values->type,
+                    'example' => $values->example ?? null,
+                    'description' => $values->description ?? null,
+                ]
+            ];
+        });
+        return [
+            'required' => true,
+            'content' => [
+                'application/json' => [
+                    'schema' => [
+                        'type' => 'object',
+                        'required' => $requiredData->pluck('key'),
+                        'properties' => $propertiesData
+                    ],
+                ]
+            ]
+        ];
+    }
+}
